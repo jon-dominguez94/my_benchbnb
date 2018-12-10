43d5a362b1d407b356cf3d5c32e21a83fb0b2c55
jondominguez94 <jon.dominguez94@yahoo.com>
2018-12-10 10:59:42 -0800

    Write sessionErrorsReducer

--- frontend/actions/session_actions.js
+++ frontend/actions/session_actions.js
@@ -2,7 +2,7 @@ import * as APIUtils from './util/session_api_util';
 
 export const RECEIVE_CURRENT_USER = "RECEIVE_CURRENT_USER";
 export const LOGOUT_CURRENT_USER = "LOGOUT_CURRENT_USER";
-export const RECEIVE_ERRORS = "RECEIVE_ERRORS";
+export const RECEIVE_SESSION_ERRORS = "RECEIVE_SESSION_ERRORS";
 
 export const login = user => dispatch => {
   return APIUtils.login(user)
@@ -31,6 +31,6 @@ export const signup = user => dispatch => {
 
 
 export const receiveErrors = errors => ({
-  type: RECEIVE_ERRORS,
+  type: RECEIVE_SESSION_ERRORS,
   errors
 });
