fa51c05406371eb5f877327cc89d977601adfe6c
jondominguez94 <jon.dominguez94@yahoo.com>
2018-12-10 09:29:36 -0800

    Setup routes

--- app/controllers/api/sessions_controller.rb
+++ app/controllers/api/sessions_controller.rb
@@ -1,5 +1,9 @@
 class Api::SessionsController < ApplicationController
   def create
+    @user = User.find_by_credentials(params[:user][:username], params[:user][:password])
+    if @user
+      login(@user)
+      redirect_to user_url
   end
 
   def destroy
--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,8 @@
 Rails.application.routes.draw do
-  namespace :api do
-    get 'sessions/create'
-    get 'sessions/destroy'
+  namespace :api, defaults: {format: json} do
+    resource :session, only: [:create, destroy]
+    resources :users, only: [:create]
   end
-  namespace :api do
-    get 'users/create'
-  end
+ 
   root to: 'static_pages#root'
 end
