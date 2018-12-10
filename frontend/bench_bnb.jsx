bab3edf01db6171c5c7857382b42b156d2fe1e19
jondominguez94 <jon.dominguez94@yahoo.com>
2018-12-10 10:13:01 -0800

    Write session_api_util

--- frontend/bench_bnb.jsx
+++ frontend/bench_bnb.jsx
@@ -1,5 +1,10 @@
 import React from 'React';
 import ReactDOM from 'react-dom';
+import * as APIUtils from './util/session_api_util';
+
+window.signup = APIUtils.signup;
+window.login = APIUtils.login;
+window.logout = APIUtils.logout;
 
 document.addEventListener("DOMContentLoaded", () => {
   const root = document.getElementById("root");
