039da6b56ee364270e53c71a5d288b9b285e94ff
jondominguez94 <jon.dominguez94@yahoo.com>
2018-12-10 09:25:43 -0800

    Wrtie application controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,10 @@
 Rails.application.routes.draw do
+  namespace :api do
+    get 'sessions/create'
+    get 'sessions/destroy'
+  end
+  namespace :api do
+    get 'users/create'
+  end
   root to: 'static_pages#root'
 end
