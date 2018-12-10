4e43ef49c7deff3fc58a9430d5156d80f009ae52
jondominguez94 <jon.dominguez94@yahoo.com>
2018-12-10 12:01:52 -0800

    Create App and root components and refactor entry point

--- frontend/bench_bnb.jsx
+++ frontend/bench_bnb.jsx
@@ -1,17 +1,14 @@
 import React from 'React';
 import ReactDOM from 'react-dom';
-import * as APIUtils from './util/session_api_util';
 import configureStore from './store/store';
-
-window.signup = APIUtils.signup;
-window.login = APIUtils.login;
-window.logout = APIUtils.logout;
+import Root from './components/root';
 
 document.addEventListener("DOMContentLoaded", () => {
   const store = configureStore();
-  window.store = store;
-  window.getState = store.getState
-  window.dispatch = store.dispatch
+
+  window.getState = store.getState;
+  window.dispatch = store.dispatch;
+
   const root = document.getElementById("root");
-  ReactDOM.render(<h1>Welcome to BenchBnB</h1>, root);
+  ReactDOM.render(<Root store={store}/>, root);
 });
